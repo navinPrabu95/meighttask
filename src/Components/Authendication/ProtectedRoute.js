59308c57333b1dad3d3460e792e782b190d18513
navinPrabu95 <navinnnivin@gmail.com>
2023-02-10 10:31:12 +0530

    sample page design changing

--- src/Components/Authendication/ProtectedRoute.js
+++ src/Components/Authendication/ProtectedRoute.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Navigate } from 'react-router'
 
+
 function ProtectedRoute(props) {
 
   const isAuthendicated = localStorage.getItem('Items')?true:false
   
-  console.log(localStorage.getItem('Items'));
 
   const component = props.children
 
   return (
-    isAuthendicated?component:<Navigate to='/signin'></Navigate>
+    isAuthendicated?component:<Navigate to='/signin'></Navigate> 
   )
 }
 
--- src/Components/Pages/SamplePage.js
+++ src/Components/Pages/SamplePage.js
@@ -1,44 +1,51 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import { Table } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import axios from 'axios'
 import './Sample.css'
 
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+
 const SamplePage = () => {
 
-    const [sampleData, setSampleData] = useState()
+    const [sampleData, setSampleData] = useState([])
+    const [page,setPage] = useState(1)
+    
 
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/todos").then(result => {
             setSampleData(result.data);
+            console.log("sampleData",result.data);
         }).catch(err => {
             console.log(err);
         })
     }, [])
 
-    const ListData = useMemo(() => {
-        return sampleData ? sampleData.map((val, i) => {
-            return <tr key={val.id}>
-                <td>{val.title}</td>
-                <td>{JSON.stringify(val.completed)}</td>
-            </tr>
-        }) : null
-    }, [sampleData])
+    const listData = useMemo(()=>{
+       return sampleData.length>0 && sampleData.slice((sampleData.length/10*page)-20,sampleData.length/10*page).map((val,i)=>{
+            return <Card key={val.id} className='sam__list'>
+                      <Card.Title>{val.title}</Card.Title>
+                      <Card.Subtitle>{JSON.stringify(val.completed)}+{val.id}</Card.Subtitle>
+               </Card>
+        })
+    },[page,sampleData])
+
+    const setPagination=(pgNo)=>{
+        setPage(pgNo)
+    }
 
     return (
         <div className='sam_container'>
             <div className='tbl_data'>
-                <h2>Sample Data From API</h2>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Completed</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {ListData}
-                    </tbody>
-                </Table>
+                {listData}
+            </div>
+            <div className='sample__padination'> 
+                { page>1 &&<span onClick={()=>setPagination(page-1)}>Prev</span> }  
+               {[...Array(sampleData.length/20)].map((_,i)=>{
+                 return <span key={i} onClick={()=>setPagination(i+1)} className={i+1===page?'active_page':'normal_page'}>{i+1}</span>
+               })} 
+               {page<10 && <span onClick={()=>setPagination(page+1)}>Next</span> } 
             </div>
         </div>
     )
